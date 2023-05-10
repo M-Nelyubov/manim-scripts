03ac8cc2998fe719d1804bb766dc596bdf57bc24
Argo <54564357+M-Nelyubov@users.noreply.github.com>
2023-05-10 07:32:41 -0400

    Created WIP but working half-unit correction video script

--- NormApprBin.py
+++ NormApprBin.py
@@ -23,6 +23,7 @@ class IncreasingN50(Scene):
                 y_length=5.5,
                 x_length=10,
                 x_axis_config={"font_size": 36},
+                bar_width=1
             )
             return new_chart, max_val
 
@@ -143,13 +144,14 @@ class HalfUnitCorrection(Scene):
                 y_range=[0, max_val, round(max_val / 5, 4)],
                 y_length=5.5,
                 x_length=10,
-                x_axis_config={"font_size": 36},
+                x_axis_config={"font_size": 24},
+                bar_width=1
             )
             return new_chart, max_val
 
         def genX(n, min_x, max_x, max_val):
             return Axes(
-                (min_x, max_x, 1),
+                (min_x - 0.5, max_x + 0.5, 1),
                 (0, max_val, 0.2),
                 tips=False,
                 axis_config={"include_numbers": True},
@@ -171,12 +173,55 @@ class HalfUnitCorrection(Scene):
         )
 
 
+
         min_x = 40
         max_x = 60
         new_chart, max_val = generateChart(self.n, self.p, min_x, max_x)
         axes = genX(self.n, min_x, max_x, max_val)
-        z_pdf = genZ(axes, self.n*self.p, (self.n*self.p*(1-self.p))**0.5)
-        self.play(Write(new_chart), Write(title), Write(z_pdf))
+
+        r_s = 45 # Real start
+        r_e = 55 # Real end
+        a_s = 45 # Approximation start
+        a_e = 55 # Approximation end
+
+        mean = self.n*self.p
+        variance = self.n * self.p * (1-self.p)
+        std = variance**0.5
+
+        z_pdf = genZ(axes, mean, std)
+        z_area = axes.get_area(z_pdf, x_range=(r_s, r_e), color=YELLOW, opacity = 0.7)
+
+        # value_text = Tex("X ~ Bin(100, 0.5) \n Y ~ N($\\mu, \\sigma$)").to_edge(RIGHT)
+        value_text = Tex("$$\int_{" + str(a_s) + "}^{" + str(a_e) + "}f_Y(x)\,dx = " + str(round(norm.cdf(a_e, mean, std) - norm.cdf(a_s, mean, std), 4)) + "$$\n" + 
+                        "$$F_{B}(" + str(a_e) + ") - F_{B}(" + str(a_s - 1) + ") = " + str(round(binom.cdf(a_e, self.n, self.p) - binom.cdf(a_s-1, self.n, self.p), 4) ) + " $$", font_size = 36).to_edge(RIGHT).shift([0,2,0])  # To the right and then up one
+
+        self.play(
+            Write(new_chart), 
+            Write(title), 
+            Write(z_pdf)
+        )
+
         self.wait()
-        # self.add(title)
+
+        self.play(
+            Write(z_area),
+            Write(value_text)
+        )
+
+        self.wait(5)
+
+        # Now make the half-unit correction
+
+        new_value_text = Tex("$$\int_{" + str(a_s-0.5) + "}^{" + str(a_e+0.5) + "}f_Y(x)\,dx = " + str(round(norm.cdf(a_e+0.5, mean, std) - norm.cdf(a_s-0.5, mean, std), 4)) + "$$\n" + 
+                             "$$F_{B}(" + str(a_e) + ") - F_{B}(" + str(a_s - 1) + ") = " + str(round(binom.cdf(a_e, self.n, self.p) - binom.cdf(a_s-1, self.n, self.p), 4) ) + " $$", font_size = 36).to_edge(RIGHT).shift([0,2,0])  # To the right and then up one
+        new_z_area = axes.get_area(z_pdf, x_range=(r_s-0.5, r_e+0.5), color=YELLOW, opacity = 0.7)
+
+        self.play(
+            ReplacementTransform(z_area, new_z_area)
+        )
+        self.play(
+            ReplacementTransform(value_text, new_value_text, run_time=0.2)
+        )
+
+        self.wait(5)
 

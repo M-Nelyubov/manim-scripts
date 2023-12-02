703bc3bfe1e879bb76f3e15cb400cac78f412bf6
Misha <mnelyubo@buffalo.edu>
2023-12-02 11:05:50 -0500

    built the 3D shape, fixed scaling, starting cross section

--- joint_distr_3d.py
+++ joint_distr_3d.py
@@ -23,6 +23,151 @@ class triangle(ThreeDScene):
         self.add(axes, base_layer)
         self.set_camera_orientation(phi=75*DEGREES, theta=-60*DEGREES, gamma=None)
 
+class SupUpperTriangle(ThreeDScene):
+    def f(self,x,y):
+        return 6*x
+
+    def construct(self):
+        x_supp = [0,1]
+        y_supp = [0,1]
+        z_range = [
+            self.f(x_supp[0], y_supp[0]),
+            self.f(x_supp[1], y_supp[1])    
+        ]
+        z_midpoint = (z_range[1] - z_range[0]) / 2
+
+        xy_step = 0.25
+        a = xy_step
+        scaling = z_range[1] / (z_range[1]+1) /2
+        axes = ThreeDAxes(
+            x_range=[-a, 1+a, a], 
+            y_range=[-a, 1+a, a],
+            z_range=[0,z_range[1]+1,1],
+            x_length=3,
+            y_length=3,
+            z_length=6
+        )
+        labels = axes.get_axis_labels(
+            Tex("x").scale(0.7), Tex("y").scale(0.45), MathTex("f_{X,Y}(x,y)").scale(0.45)
+        )
+
+        distribution_layer = Surface(
+            lambda u, v: np.array([
+                u*v,
+                v,
+                v*self.f(u,v) * scaling   # Spaghetti Code
+            ]), v_range=y_supp, u_range=x_supp,
+            fill_opacity = 0.5,
+            checkerboard_colors=[RED_D, RED_E], resolution=(1, 1)
+        )
+        base_layer = Surface(
+            lambda u, v: np.array([
+                u*v,
+                v,
+                0
+            ]), v_range=y_supp, u_range=x_supp,
+            fill_opacity = 0.5,
+            checkerboard_colors=[RED_D, RED_E], resolution=(1, 1)
+        )
+        y1_border = Surface(
+            lambda u, v: np.array([
+                u,
+                1,
+                v * self.f(u,v) * scaling
+            ]), v_range=y_supp, u_range=x_supp,
+            fill_opacity = 0.5,
+            checkerboard_colors=[RED_D, RED_E], resolution=(1, 1)
+        )
+        diag_border = Surface(
+            lambda u, v: np.array([
+                u,
+                u,
+                v * self.f(u,v) * scaling
+            ]), v_range=y_supp, u_range=x_supp,
+            fill_opacity = 0.5,
+            checkerboard_colors=[RED_D, RED_E], resolution=(1, 1)
+        )
+
+        geometry = VGroup()
+        geometry.add(
+            distribution_layer, 
+            base_layer, 
+            y1_border, 
+            diag_border
+        )
+        geometry.move_to(axes.coords_to_point(
+            (x_supp[1] - x_supp[0]) / 2,
+            (y_supp[1] - y_supp[0]) / 2,
+            z_midpoint
+        )).scale(2)
+
+        # self.renderer.camera.light_source.move_to(3*IN) # changes the source of the light
+        # self.set_camera_orientation(phi=75 * DEGREES, theta=30 * DEGREES)
+        self.play(Write(axes), Write(labels))
+        
+        self.play(Write(base_layer))
+
+        self.move_camera(
+            phi   =  75 * DEGREES, 
+            theta = -90 * DEGREES, 
+            gamma = None,
+            frame_center=axes
+        )
+
+        self.wait()
+        self.play(ReplacementTransform(base_layer, geometry))
+        self.wait()
+
+        # self.play(Write(x_min_layer, run_time = 1.35))
+
+        # Do one full rotation around the object
+        play_time = 6
+        r = 2 * PI / play_time
+        self.begin_ambient_camera_rotation(rate=r)
+        self.wait(play_time)
+        self.stop_ambient_camera_rotation()
+
+        self.move_camera(
+            phi   =  90 * DEGREES, 
+            theta = (360-90) * DEGREES, 
+            gamma = None,
+            frame_center=axes
+        )
+
+        t = 360
+
+        # Show just the four corners
+        self.wait()
+        self.move_camera(theta = (t+0) * DEGREES)
+        # self.wait()
+        # self.move_camera(theta = (t+90) * DEGREES)
+        self.wait(3)
+
+        self.move_camera(
+            phi   =  70 * DEGREES, 
+            theta = (360-90-20) * DEGREES, 
+            gamma = None,
+            frame_center=axes
+        )
+
+        self.wait(3)
+
+        x=0
+        cross_section = Surface(
+            lambda u, v: np.array([
+                x,
+                u,
+                v * scaling
+            ]), v_range=[z_range[0]-4, z_range[1]+4], u_range=[-1.5,2.5],
+            fill_opacity = 0.75,
+            checkerboard_colors=[YELLOW, YELLOW], resolution=(1, 1)
+        ).move_to(axes.coords_to_point(0, 0.5, z_midpoint))
+
+        self.play(Write(cross_section))
+
+        self.wait(5)
+
+
 class Sup3d(ThreeDScene):
     def f(self,x,y):
         if x > 2 or x < 0:

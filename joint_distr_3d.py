6e747c9a8bd423f35ada96704dca118cf31a8560
Misha <mnelyubo@buffalo.edu>
2024-01-10 23:11:06 -0500

    the same process for some fixed value of y

--- joint_distr_3d.py
+++ joint_distr_3d.py
@@ -3,7 +3,7 @@ from manim import *
 epsilon = 0.01
 ep = epsilon
 
-class SupUpperTriangle(ThreeDScene):
+class SupUpperTriangleY(ThreeDScene):
     def f(self,x,y):
         return 6*x
 
@@ -150,56 +150,54 @@ class SupUpperTriangle(ThreeDScene):
 
         self.wait(2)
 
-        x=0.6   # the value of x at which the support will be cut
+        y=0.5   # the value of x at which the support will be cut
         cross_section = Surface(
-            lambda u, v: axes.c2p(x, u, v),
-            u_range=[y_supp[0]-.5, y_supp[1]+.5],
+            lambda u, v: axes.c2p(u, y, v),
+            u_range=[x_supp[0]-.5, x_supp[1]+.5],
             v_range=[z_range[0]-1, z_range[1]+2], 
             fill_opacity = 1,
             checkerboard_colors=[YELLOW, YELLOW], resolution=(1, 1)
         )
 
         self.play(Write(cross_section))
-       
+
         # The upper and lower halves of the support after it is cut at the cross section
         lower_supp = Polyhedron([
             axes.c2p(0,0,0),  # 0 - origin
-            axes.c2p(0,1,0),  # 1 - upper left  corner of the left trapezoid
-            axes.c2p(x,1,0),  # 2 - upper right corner of the left trapezoid
-            axes.c2p(x,x,0),  # 3 - lower right corner of the left trapezoid
+            axes.c2p(0,y,0),  # 1 - upper left  corner of the bottom triangle
+            axes.c2p(y,y,0),  # 2 - upper right corner of the bottom triangle
 
-            axes.c2p(x,x,self.f(x,x)), # 4 - lower right peak point
-            axes.c2p(x,1,self.f(x,1))  # 5 - upper right peak point
+            axes.c2p(y,y,self.f(y,y))  # 3 - top of the corner
         ],[
-            [0,1,2,3],  # lower left support area
-            [0,3,4],    # front face
-            [1,2,5],    # rear face
-            [0,1,5,4]   # top
+            [0,1,2],  # base triangle
+            [0,1,3],  # support top
+            [0,3,2],  # lower edge of the support
+            [1,2,3]   # upper edge of the support
         ], graph_config={"vertex_config":{'radius': 0.0001}})
 
         upper_supp =  Polyhedron([
-            axes.c2p(x,1,0),  # 0 - upper left  corner of the right triangle
-            axes.c2p(x,x,0),  # 1 - lower left  corner of the right triangle
-            axes.c2p(1,1,0),  # 2 - upper right corner of the right triangle
+            axes.c2p(0,y,0),  # 0 - lower left  corner of the upper trapezoid
+            axes.c2p(y,y,0),  # 1 - lower right corner of the upper trapezoid
+            axes.c2p(1,1,0),  # 2 - upper right corner of the upper trapezoid
+            axes.c2p(0,1,0),  # 3 - upper left  corner of the upper trapezoid
 
-            axes.c2p(x,x,self.f(x,x)), # 3 - lower left peak point
-            axes.c2p(x,1,self.f(x,1)), # 4 - upper left peak point
+            axes.c2p(y,y,self.f(y,y)), # 4 - lower left peak point
             axes.c2p(1,1,self.f(1,1))  # 5 - global peak point
         ],[
-            [0,1,2],    # upper right support area
-            [1,3,5,2],  # front face
-            [0,4,5,2],  # rear face
-            [3,4,5]     # top
+            [0,1,2,3], # base of the support
+            [0,1,4],   # bottom face
+            [1,2,5,4], # y=x face
+            [2,3,5],   # y=1 face
+            [0,3,5,4]  # distribution face
         ], graph_config={"vertex_config":{'radius': 0.0001}})
 
         # The cross-section is replaced by the support intersect the cross section to get the slice of interest
         slice =  Polyhedron([
-            axes.c2p(x,1,0),  # 0 - higher base
-            axes.c2p(x,x,0),  # 1 - lower base
-            axes.c2p(x,x,self.f(x,x)), # 2 - lower peak point
-            axes.c2p(x,1,self.f(x,1)) # 3 - upper peak point
+            axes.c2p(0,y,0),  # 0 - lower  end at x=0
+            axes.c2p(y,y,0),  # 1 - higher end at x=y
+            axes.c2p(y,y,self.f(y,y)), # 2 - peak point at x=y
         ],[
-            [0,1,2,3]    # the slice
+            [0,1,2]    # the slice
         ], graph_config={"vertex_config":{'radius': 0.0001}}).set_color(YELLOW)
 
         # Since all of these are by default polygons with non-zero node sizes, 
@@ -221,10 +219,10 @@ class SupUpperTriangle(ThreeDScene):
 
         # Move the components of the split shape apart
         slice.generate_target()
-        slice.target.shift(RIGHT*1)
+        slice.target.shift(UP*1)
 
         upper_supp.generate_target()
-        upper_supp.target.shift(RIGHT*2)
+        upper_supp.target.shift(UP*2)
 
         self.play(MoveToTarget(upper_supp), MoveToTarget(slice))
 
@@ -240,32 +238,32 @@ class SupUpperTriangle(ThreeDScene):
 
         old_slice = slice
 
-        marginal_plot = Axes([y_supp[0], y_supp[1]+2*a, a], z_range, 
+        marginal_plot = Axes([x_supp[0], x_supp[1]+2*a, a], z_range, 
                     axis_config={"include_numbers": True},
                 ).shift(RIGHT*4).scale(0.5)
         self.writeFixed(marginal_plot)
         # self.add_fixed_in_frame_mobjects(slice)
         # self.play(ReplacementTransform(old_slice, slice))
 
-        def fy(y):
+        def fx(x):
             if y<x or y>1:
                 return 0
             return self.f(x,y)
 
-        def fygx(y,x):
-            return self.f(x,y) / (6*x * (1-x))
+        def fxgy(y,x):
+            return self.f(x,y) / (3*y*y)
 
         color = YELLOW
-        x_label = MathTex("y").scale(0.5)
-        y1_label = MathTex("f_{XY}(x="+str(x)+",y)").scale(0.5).set_color(color)
+        x_label = MathTex("x").scale(0.5)
+        y1_label = MathTex("f_{XY}(x,y="+str(y)+")").scale(0.5).set_color(color)
 
         labels_2d = marginal_plot.get_axis_labels(x_label, y1_label)
         self.writeFixed(labels_2d)
 
-        und1 = marginal_plot.plot(lambda t: 0, x_range=[0,x], color=color)
-        sup1 = marginal_plot.plot(fy, x_range=[x,1], color=color)
-        auc1 = marginal_plot.get_area(sup1, x_range=[x,1], color=color) # Area under the curve
-        ovr1 = marginal_plot.plot(lambda t: 0, x_range=[1,1.5], color=color)
+        und1 = marginal_plot.plot(lambda t:0, x_range=[-0.1,0], color=color)
+        sup1 = marginal_plot.plot(fx, x_range=[0,y], color=color)
+        auc1 = marginal_plot.get_area(sup1, x_range=[0,y], color=color) # Area under the curve
+        ovr1 = marginal_plot.plot(lambda t: 0, x_range=[y,1.5], color=color)
         marginal1 = VGroup(y1_label, und1, auc1, ovr1)
         # self.add_fixed_in_frame_mobjects(simplified)
 
@@ -279,30 +277,30 @@ class SupUpperTriangle(ThreeDScene):
         self.wait(0.5)
 
         # show the constant on the farthest left
-        formular0 = MathTex("x="+str(x)).move_to(LEFT * 4 + UP*3).scale(0.5)
+        formular0 = MathTex("y="+str(y)).move_to(LEFT * 4 + UP*3).scale(0.5)
         self.writeFixed(formular0)
 
-        scale = 6 * x * (1-x)
+        scale = 3 * y * y # the integral of the 
 
-        formular1 = MathTex("f_{X}("+str(x)+") = \int _{y=-\infty}^{y=\infty} f_{XY}(x="+str(x)+", y) dy = \int _{y="+str(x)+"}^{y=1} 6x dy = 6xy|_{"+str(x)+"}^{1}   =  6("+str(x)+") (1-("+str(x)+")) = "+str(scale))
+        formular1 = MathTex("f_{Y}("+str(y)+") = \int _{x=-\infty}^{x=\infty} f_{XY}(x, y="+str(y)+") dx = \int _{x=0}^{x="+str(y)+"} 6x dx = \\frac{6}{2}x^2|_{0}^{"+str(y)+"}   =  3("+str(y)+"^2) = "+str(scale))
         formular1.scale(0.5).move_to(RIGHT *2 +  UP * 3)
         self.writeFixed(formular1)
 
         self.wait(3)
 
-        formular2 = MathTex("f_{X}("+str(x)+") ="+str(6 * x * (1-x)))
+        formular2 = MathTex("f_{Y}("+str(y)+") ="+str(scale))
         formular2.scale(0.5).move_to(LEFT * 2 +  UP * 3)
         self.replaceFixed(formular1, formular2)
 
         self.wait(3)
  
-        formular3 = MathTex("f_{Y|x="+str(x)+"}(y)"," = \\frac{f_{XY}("+str(x)+",y)}{f_X("+str(x)+")}"," = \\frac{f_{XY}("+str(x)+",y)}{"+str(scale)+"}")
+        formular3 = MathTex("f_{X|y="+str(y)+"}(x)"," = \\frac{f_{XY}(x,"+str(y)+")}{f_Y("+str(y)+")}"," = \\frac{f_{XY}(x,"+str(y)+")}{"+str(scale)+"}")
         formular3.scale(0.5).move_to(RIGHT *3 +  UP * 3)
         self.writeFixed(formular3)
 
         self.wait(1)
 
-        y2_label = MathTex("f_{Y|x="+str(x)+"}(y)").scale(0.5).set_color(RED)
+        y2_label = MathTex("f_{X|y="+str(y)+"}(x)").scale(0.5).set_color(RED)
         y2_label.next_to(y1_label,UP)
         self.writeFixed(y2_label)
 
@@ -323,10 +321,10 @@ class SupUpperTriangle(ThreeDScene):
 
         # Draw the normalized marginal distribution for this value of x
         color = RED
-        und2 = marginal_plot.plot(lambda t: 0, x_range=[0,x], color=color)
-        sup2 = marginal_plot.plot(lambda y: fygx(y,x), x_range=[x,1], color=color)
-        auc2 = marginal_plot.get_area(sup2, x_range=[x,1], color=color)
-        ovr2 = marginal_plot.plot(lambda t: 0, x_range=[1,1.5], color=color)
+        und2 = marginal_plot.plot(lambda t: 0, x_range=[-0.25,0], color=color)
+        sup2 = marginal_plot.plot(lambda x: fxgy(y,x), x_range=[0,y], color=color)
+        auc2 = marginal_plot.get_area(sup2, x_range=[0,y], color=color)
+        ovr2 = marginal_plot.plot(lambda t: 0, x_range=[y,1.5], color=color)
 
         self.writeFixed(und2, pause_time=0.5)
         self.writeFixed(auc2, pause_time=0.5)
